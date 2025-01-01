687e3c4211a95120201e663f699349899f561b00
mona <manonaa43@gmail.com>
2025-01-01 20:36:10 +0200

    ةةةةةةةةةةةةةةة

--- src/Pages/1-Home/CompanyComponentsCards.jsx
+++ src/Pages/1-Home/CompanyComponentsCards.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-
+import { useNavigate } from 'react-router-dom';
 const CompanyComponentsCards = ({img ,title, description, url}) => {
+  const navigate = useNavigate();
   return (
     <card className="shadow-lg col-lg-3   card1">
     <img className="mx-auto d-flex my-3" src={img} alt="" width={"100%"} />
     <h5 className="text-center"> {title}</h5>
     <p style={{textAlign:"center"}}> {description}</p>
     <button className='btn2 my-3'onClick={()=>{
-      document.location.href = '/'+url
+      navigate('/' + url);
     }}> المزيد </button>
    </card>
   )
